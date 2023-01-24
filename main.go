db5e190222f1dbdb1dc634197a3500cd7f5ff9b4
berke <berke.ahlatci@gmail.com>
2023-01-24 16:57:39 +0300

    implemented query filter for "GET /api/articles"

--- main.go
+++ main.go
@@ -3,63 +3,62 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"strconv"
 )
 
-
-type User struct{
+type User struct {
 	Username string `json:"username"`
-	Email string `json:"email"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
 	Password string `json:"password"`
-	Bio string `json:"bio"`
-	Image string `json:"image"`
+	Bio      string `json:"bio"`
+	Image    string `json:"image"`
 }
 
-type Profile struct{
-	Username string `json:"username"`
-	Bio string `json:"bio"`
-	Image string `json:"image"`
-	Following bool `json:"following"`
+type Profile struct {
+	Username  string `json:"username"`
+	Bio       string `json:"bio"`
+	Image     string `json:"image"`
+	Following bool   `json:"following"`
 }
 
-type Article struct{
-	Title string `json:"title"`
-	Description string `json:"description"`
-	Body string `json:"body"`
-	TagList []string `json:"tagList"`
-	Comments []Comment 
-	Slug string `json:"slug"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-	Favorited bool `json:"favorited"`
-	FavoritesCount int `json:"favoritesCount"`
-	Author *Profile `json:"author"`
+type Article struct {
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	Body           string    `json:"body"`
+	TagList        []string  `json:"tagList"`
+	Comments       []Comment `json:"comments"`
+	Slug           string    `json:"slug"`
+	CreatedAt      string    `json:"createdAt"`
+	UpdatedAt      string    `json:"updatedAt"`
+	Favorited      bool      `json:"favorited"`
+	FavoritesCount int       `json:"favoritesCount"`
+	Author         *Profile  `json:"author"`
 }
 
-type Comment struct{
-	Id int `json:"id"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-	Body string `json:"body"`
-	Author *Profile `json:"author"`
-	Slug string
+type Comment struct {
+	Id        int      `json:"id"`
+	CreatedAt string   `json:"createdAt"`
+	UpdatedAt string   `json:"updatedAt"`
+	Body      string   `json:"body"`
+	Author    *Profile `json:"author"`
+	Slug      string
 }
 
-type MultipleComments struct{
+type MultipleComments struct {
 	Comments []Comment `json:"comments"`
 }
 
 var profiles []Profile
 
-var comments[]Comment
+var comments []Comment
 var tags []string
 var articles []Article
 
-
-func getProfile(w http.ResponseWriter, r *http.Request){
+func getProfile(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
@@ -71,36 +70,95 @@ func getProfile(w http.ResponseWriter, r *http.Request){
 	}
 }
 
-func getArticles(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(articles)
+func getArticles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	limit := 20
+	offset := 0
+
+	queries := r.URL.Query()
+	tag := queries.Get("tag")
+	author := queries.Get("author")
+	favorited := queries.Get("favorited")
+	queryLimit := queries.Get("limit")
+	queryOffset := queries.Get("offset")
+
+	if queryLimit != "" {
+		limit, _ = strconv.Atoi(queryLimit)
+	}
+	if queryOffset != "" {
+		offset, _ = strconv.Atoi(queryOffset)
+	}
+
+	if offset+limit >= len(articles) {
+		limit = len(articles) - offset
+	}
+
+	if tag != "" {
+		newArticles := []Article{}
+
+		for _, article := range articles {
+			for _, t := range article.TagList {
+				if t == tag {
+					newArticles = append(newArticles, article)
+				}
+			}
+		}
+		json.NewEncoder(w).Encode(newArticles[offset : offset+limit])
+		return
+	}
+
+	if author != "" {
+		newArticles := []Article{}
+
+		for _, article := range articles {
+			if article.Author.Username == author {
+				newArticles = append(newArticles, article)
+			}
+		}
+		json.NewEncoder(w).Encode(newArticles[offset : offset+limit])
+		return
+	}
+
+	if favorited != "" {
+		newArticles := []Article{}
+
+		for _, article := range articles {
+			if article.Favorited {
+				newArticles = append(newArticles, article)
+			}
+		}
+		json.NewEncoder(w).Encode(newArticles[offset : offset+limit])
+		return
+	}
+
+	json.NewEncoder(w).Encode(articles[offset : offset+limit])
 }
 
-func getArticle(w http.ResponseWriter, r *http.Request){
+func getArticle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-
-	for _,article := range articles{
-		if article.Slug == params["slug"]{
+	for _, article := range articles {
+		if article.Slug == params["slug"] {
 			json.NewEncoder(w).Encode(article)
 			return
 		}
 	}
 }
 
-func getTags(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
+func getTags(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(tags)
 }
 
-func getComments(w http.ResponseWriter, r *http.Request){
+func getComments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
+
 	params := mux.Vars(r)
-	
-	for _, article := range articles{
-		if article.Slug == params["slug"]{
+
+	for _, article := range articles {
+		if article.Slug == params["slug"] {
 
 			var coms MultipleComments
 
@@ -112,20 +170,21 @@ func getComments(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
 
+}
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 
-	profiles = append(profiles, Profile{Username: "berke", Bio:"I am a student.", Image:"", Following: false})
-	profiles = append(profiles, Profile{Username: "keskul", Bio:"I am a cat.", Image:"", Following: false})
+	profiles = append(profiles, Profile{Username: "berke", Bio: "I am a student.", Image: "", Following: false})
+	profiles = append(profiles, Profile{Username: "keskul", Bio: "I am a cat.", Image: "", Following: false})
 
 	articles = append(articles, Article{Slug: "how-to-train-your-dragon", Title: "How to train your dragon", Description: "Ever wonder how?", Body: "It takes a Jacobian", TagList: []string{"dragons", "training"}, CreatedAt: "2016-02-18T03:22:56.637Z", UpdatedAt: "2016-02-18T03:48:35.824Z", Favorited: false, FavoritesCount: 0, Author: &profiles[0]})
 
-	comments = append(comments, Comment{Id: 1, CreatedAt: "2016-02-18T03:22:56.637Z", UpdatedAt: "2016-02-18T03:48:35.824Z", Body:"Nice post, thanks!", Author: &profiles[1], Slug: "how-to-train-your-dragon"})
+	comments = append(comments, Comment{Id: 1, CreatedAt: "2016-02-18T03:22:56.637Z", UpdatedAt: "2016-02-18T03:48:35.824Z", Body: "Nice post, thanks!", Author: &profiles[1], Slug: "how-to-train-your-dragon"})
 
-	articles[0].Comments = append(articles[0].Comments, Comment{Id: 1, CreatedAt: "2016-02-18T03:22:56.637Z", UpdatedAt: "2016-02-18T03:48:35.824Z", Body:"Nice post, thanks!", Author: &profiles[1], Slug: "how-to-train-your-dragon"})
-
+	articles[0].Comments = append(articles[0].Comments, Comment{Id: 1, CreatedAt: "2016-02-18T03:22:56.637Z", UpdatedAt: "2016-02-18T03:48:35.824Z", Body: "Nice post, thanks!", Author: &profiles[1], Slug: "how-to-train-your-dragon"})
 
 	tags = append(tags, "dragons")
 	tags = append(tags, "training")
@@ -136,6 +195,8 @@ func main(){
 	r.HandleFunc("/api/articles/{slug}/comments", getComments).Methods("GET")
 	r.HandleFunc("/api/tags", getTags).Methods("GET")
 
+	r.HandleFunc("/api/user/login", login).Methods("POST")
+
 	fmt.Println("Starting server at port 8000")
-	log.Fatal(http.ListenAndServe(":8000",r))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
